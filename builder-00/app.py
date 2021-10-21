b471b6c1d985b32d949b281a4787916a19561e00
Jeremy Spewock <jspewock@d122223.iol.unh.edu>
2021-10-21 12:28:24 -0400

    Moved global processes into if main statement
    *moved all processes and variables that don't need to be global into the
    if main statement to make for easier importing
    *changed all variables and function into snake case instead of a mix
    between snake and camel case
    
    Signed-off-by: Jeremy Spewock <jspewock@d122223.iol.unh.edu>

--- builder-00/app.py
+++ builder-00/app.py
@@ -4,29 +4,6 @@ import os
 import sys
 
 app = Flask(__name__)
-#basically what this does is it looks at the argumens given to the python3 bash command when you use it on
-#the file, which will always be at least one because the file name is an argument, and then it checks to see
-#if there are more than one, if there are, is the second one an integer?
-if len(sys.argv) > 1:
-    try:
-        int(sys.argv[1])
-    #if the argument is not an integer
-    except:
-        #found how to raise errors from https://stackoverflow.com/questions/2052390/manually-raising-throwing-an-exception-in-python
-        raise ValueError("Invalid port. Port must be in integer greater than 0.")
-
-    #if the argument is an integer and is greater than 0
-    if int(sys.argv[1]) > 0:
-        PORT = sys.argv[1]
-    else:
-        raise ValueError("Invalid port. Port must be in integer greater than 0.")
-
-#if the command doesn't have an additional argument
-else:
-    raise ReferenceError("Port required. Command should be, \"python3 app.py <PORT>\"")
-
-serverName = 'builder-00.ofa.iol.unh.edu:' + str(PORT)
-app.config['SERVER_NAME'] = serverName
 
 dhcpd_scripts_variable = "dhcpd_scripts"
 
@@ -34,17 +11,17 @@ dhcpd_scripts_variable = "dhcpd_scripts"
 #    get a list of file names
 #--------------------------------
 @app.route('/hosts.d', methods=["GET"])
-def getFileNames():
+def get_file_names():
     #gets file names from specified path
     #docs.python.org/3/library/os.html
-    fileNameList = os.listdir('/var/lib/tftpboot/hosts.d/')
-    return jsonify({"fileNames": fileNameList}), 200
+    file_name_list = os.listdir('/var/lib/tftpboot/hosts.d/')
+    return jsonify({"fileNames": file_name_list}), 200
 
 #--------------------------------
 #        restart dhcp4
 #--------------------------------
 @app.route('/restartDhcp4Service', methods=["POST"])
-def restartDhcp():
+def restart_dhcp():
     query = subprocess.run(['sudo', os.path.abspath(os.path.dirname(__file__)) + "/" + dhcpd_scripts_variable +  '/restart_dhcp.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     #if the command returns without error
     if query.returncode == 0:
@@ -56,7 +33,7 @@ def restartDhcp():
 #        restart dhcp6
 #--------------------------------
 @app.route('/restartDhcp6Service', methods=["POST"])
-def restartdhcp6():
+def restart_dhcp6():
     query = subprocess.run(['sudo', os.path.abspath(os.path.dirname(__file__)) + "/" + dhcpd_scripts_variable + '/restart_dhcp6.sh'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     if query.returncode == 0:
         return jsonify({"status": 200, "message": "dhcp6 was restarted successfully"}), 200
@@ -67,7 +44,7 @@ def restartdhcp6():
 #       check dhcp status
 #--------------------------------
 @app.route('/checkDhcpStatus', methods=["POST"])
-def checkDhcp():
+def check_dhcp():
     #bash commands to check status of both dhcp4 and dhcp6
     #found how to use os.path,abspath from https://stackoverflow.com/questions/57311876/os-getcwd-returns-a-slash
     #had to use this method because os.getcwd was working inconsistently
@@ -87,19 +64,19 @@ def checkDhcp():
         dhcp6_status_exists = True
 
     #function to format the outputs of bash commands
-    def formatDhcp(input):
-        dhcpString = str(input)
-        dhcpArr = dhcpString.split('\\n')
-        dhcpOutput = dhcpArr[2].strip()
-        dhcpOutput = dhcpOutput.replace("Active", "Status", 1)
-        return dhcpOutput
+    def format_dhcp(input):
+        dhcp_string = str(input)
+        dhcp_arr = dhcp_string.split('\\n')
+        dhcp_output = dhcp_arr[2].strip()
+        dhcp_output = dhcp_output.replace("Active", "Status", 1)
+        return dhcp_output
 
     #if both dchp4 and dhcp6 have a status 
     if dhcp4_status_exists and dhcp6_status_exists:
-        return jsonify({"status": 200, "message": {"dhcp4Status": formatDhcp(query.stdout), "dhcp6Status": formatDhcp(query2.stdout)}}), 200
+        return jsonify({"status": 200, "message": {"dhcp4Status": format_dhcp(query.stdout), "dhcp6Status": formatDhcp(query2.stdout)}}), 200
     #if only dhcp4 has a status
     elif dhcp4_status_exists and dhcp6_status_exists != True:
-        return jsonify({"status": 206, "message": {"dhcp4Status": formatDhcp(query.stdout), "dhcp6Status": "Unknown"}}), 206
+        return jsonify({"status": 206, "message": {"dhcp4Status": format_dhcp(query.stdout), "dhcp6Status": "Unknown"}}), 206
     #if only dhcp6 has a status
     elif dhcp6_status_exists and dhcp4_status_exists != True:
         return jsonify({"status": 206, "message": {"dhcp4Status": "Unknown", "dhcp6Status": formatDhcp(query2.stdout)}}), 206
@@ -111,18 +88,18 @@ def checkDhcp():
 #     Rebuild DHCP Config
 #------------------------------
 @app.route('/rebuildDhcp', methods=["POST"])
-def rebuildDhcp():
+def rebuild_dhcp():
     #essentially, this try statement says if we found the node files, continue, if not, throw an error
     try:
         #creates a list of file names from the given path
-        fileNameList = os.listdir('/var/lib/tftpboot/hosts.d/')
+        file_name_list = os.listdir('/var/lib/tftpboot/hosts.d/')
     except:
         return jsonify({"status": 502, "message": "Unable to find node configuration files"}), 502
     #bash command resets the config file with a new template
     query = subprocess.run(["sudo", os.path.abspath(os.path.dirname(__file__)) + "/" + dhcpd_scripts_variable + "/reset_dhcpd_config.sh"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     #if the script ran without error
     if query.returncode == 0:
-        for node in fileNameList:
+        for node in file_name_list:
             query2 = subprocess.run(["sudo", os.path.abspath(os.path.dirname(__file__)) + "/" + dhcpd_scripts_variable + "/rebuild_dhcp.sh", ""+node], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         return jsonify({"status": 200, "message": "Rebuilt dhcpd configuration successfully"}), 200
     else:
@@ -134,13 +111,13 @@ def rebuildDhcp():
 #      delete a node file
 #------------------------------
 @app.route('/hosts.d/<fileName>', methods=["DELETE"])
-def deleteAFile(fileName):
+def delete_a_file(file_name):
     #creates a list of file names from the given path and then uses the .count method to see if the given
     #parameter matches one of the file names
-    fileNameList = os.listdir('/var/lib/tftpboot/hosts.d/')
-    fileNameCheck = fileNameList.count(fileName)
-    if fileNameCheck == 1:
-        query = subprocess.run(["sudo", os.path.abspath(os.path.dirname(__file__)) + "/" + dhcpd_scripts_variable + "/rm_dhcp_file.sh", ""+fileName], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    file_name_list = os.listdir('/var/lib/tftpboot/hosts.d/')
+    file_name_check = file_name_list.count(file_name)
+    if file_name_check == 1:
+        query = subprocess.run(["sudo", os.path.abspath(os.path.dirname(__file__)) + "/" + dhcpd_scripts_variable + "/rm_dhcp_file.sh", ""+file_name], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         #if there were no errors
         if query.returncode == 0:
             return jsonify({"status": 200, "message": "File was deleted"}), 200
@@ -152,4 +129,27 @@ def deleteAFile(fileName):
         return jsonify({"status":404, "message": "File was not found"}), 404
 
 if __name__ == "__main__":
+    #basically what this does is it looks at the argumens given to the python3 bash command when you use it on
+    #the file, which will always be at least one because the file name is an argument, and then it checks to see
+    #if there are more than one, if there are, is the second one an integer?
+    if len(sys.argv) > 1:
+        try:
+            int(sys.argv[1])
+        #if the argument is not an integer
+        except:
+            #found how to raise errors from https://stackoverflow.com/questions/2052390/manually-raising-throwing-an-exception-in-python
+            raise ValueError("Invalid port. Port must be in integer greater than 0.")
+
+        #if the argument is an integer and is greater than 0
+        if int(sys.argv[1]) > 0:
+            PORT = sys.argv[1]
+        else:
+            raise ValueError("Invalid port. Port must be in integer greater than 0.")
+
+        #if the command doesn't have an additional argument
+    else:
+        raise ReferenceError("Port required. Command should be, \"python3 app.py <PORT>\"")
+
+    server_name = 'builder-00.ofa.iol.unh.edu:' + str(PORT)
+    app.config['SERVER_NAME'] = server_name
     app.run(port=PORT)
