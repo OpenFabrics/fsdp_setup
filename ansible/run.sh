51b8d99bbae384e1cc298f1fd9774715839a5f6e
Cody Cheng <ccheng@iol.unh.edu>
2024-10-14 17:49:36 -0400

    Run Ansible with Kickstart
    Signed-off-by: Cody Cheng <ccheng@iol.unh.edu>

--- ansible/run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export ANSIBLE_HOST_KEY_CHECKING=False
-cd /root/fsdp_setup/ansible
-
-ansible-playbook -i inventory --diff -v setup_host.yml --limit "$(hostname)" --connection=local
-
-systemctl disable run_fsdp_setup.service
-
-reboot
