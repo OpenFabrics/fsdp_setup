e6194592043a202db673f6abe95d4af5e2e5d49e
Cody Cheng <ccheng@iol.unh.edu>
2024-10-14 15:11:01 -0400

    Fix nmcli wrong interface path
    Signed-off-by: Cody Cheng <ccheng@iol.unh.edu>

--- ansible/setup.sh
+++ ansible/setup.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+
 export LC_ALL=en_US.UTF-8
 export LANGUAGE=en_US.UTF-8
 export LANG=en_US.UTF-8
+export ANSIBLE_HOST_KEY_CHECKING=False
 
 yum install -y python3 python3-pip
 python3 -m pip install --upgrade pip
-python3 -m pip install wheel "ansible>=4"
+python3 -m pip install --upgrade wheel pyopenssl "ansible>=4"
 mv /usr/local/bin/ansible* /usr/bin
 
 cd /root/fsdp_setup/ansible
 
-chmod +x run.sh
-cp run_fsdp_setup.service /etc/systemd/system/run_fsdp_setup.service
-
-systemctl enable run_fsdp_setup.service
+ansible-playbook -i inventory --diff -v setup_host.yml --limit "$(hostname)" --connection=local
